ea788651c9f715ac6f5763e9245a43ca2519ce84
Gowtham2005-2005 <gowthamsm2005@gmail.com>
2025-02-17 18:12:23 +0530

    bug fixes-register-route

--- components/ui/textarea2.tsx
+++ components/ui/textarea2.tsx
@@ -3,58 +3,58 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
 
-type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
-    const radius = 100; 
+    const radius = 100; // Change this to increase the radius of the hover effect
     const [visible, setVisible] = React.useState(false);
 
     const mouseX = useMotionValue(0);
     const mouseY = useMotionValue(0);
 
-    const handleMouseMove = (e: React.MouseEvent<HTMLTextAreaElement>) => {
-      const { left, top } = e.currentTarget.getBoundingClientRect();
-      const x = e.clientX - left;
-      const y = e.clientY - top;
+    function handleMouseMove({ currentTarget, clientX, clientY }: { currentTarget: HTMLElement, clientX: number, clientY: number }) {
+      const { left, top } = currentTarget.getBoundingClientRect();
 
-      mouseX.set(x);
-      mouseY.set(y);
-    };
+      mouseX.set(clientX - left);
+      mouseY.set(clientY - top);
+    }
 
     return (
       <motion.div
         style={{
           background: useMotionTemplate`
-            radial-gradient(
-              ${visible ? radius + "px" : "0px"} at ${mouseX}px ${mouseY}px,
-              var(--blue-500),
-              transparent 80%
-            )
-          `,
+        radial-gradient(
+          ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
+          #EB0028,
+          transparent 80%
+        )
+      `,
         }}
-        className="group/input relative rounded-lg p-[2px] transition duration-300"
+        onMouseMove={handleMouseMove}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+        className="p-[2px] rounded-lg transition duration-300 group/textarea"
       >
         <textarea
           className={cn(
-            "flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            `flex w-full border-none bg-zinc-800 text-white shadow-input rounded-md px-3 py-2 text-sm min-h-[100px] resize-none
+          file:border-0 file:bg-transparent file:text-sm file:font-medium 
+          placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-600
+          disabled:cursor-not-allowed disabled:opacity-50
+          shadow-[0px_0px_1px_1px_var(--neutral-700)]
+          group-hover/textarea:shadow-none transition duration-400
+          `,
             className
           )}
           ref={ref}
-          onMouseMove={handleMouseMove}
-          onMouseEnter={() => {
-            setVisible(true);
-          }}
-          onMouseLeave={() => {
-            setVisible(false);
-          }}
           {...props}
         />
       </motion.div>
     );
   }
 );
-
 Textarea.displayName = "Textarea";
 
-export default Textarea;
+export { Textarea };
